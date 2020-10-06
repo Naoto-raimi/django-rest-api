b7e6a174ec682330e88ed3dac6850415a8224ac7
naoto tanaka <7010raimi@gmail.com>
2020-10-06 23:41:28 +0900

    [add] views

---
+++ api/urls.py
@@ -0,0 +1,13 @@
+from rest_framework import routers
+from django.urls import path
+from django.conf.urls import include
+from api.views import TaskViewSet, UserViewSet, ManageUserView
+
+router = routers.DefaultRouter()
+router.register('tasks', TaskViewSet)
+router.register('users', UserViewSet)
+
+urlpatterns = [
+    path('myself/', ManageUserView.as_view(), name='myself'),
+    path('',include(router.urls))
+]
